bb59787560ef06639408c18a435659da5b0ff1e0
Ramroop <alyssa.ramroop@fiserv.com>
2023-10-10 16:39:11 -0400

    m

---
+++ solutions.java
@@ -0,0 +1,26 @@
+
+
+public class solutions {
+
+
+    public static boolean isPrime(int x){
+        if (x < 2) {
+            return false;
+        }
+        for (int i = 2; i < x; i++){
+            if (x % i == 0){
+                return false;
+            }
+        }  
+        return true; 
+    }
+
+
+    public static void main(String [] args){
+        System.out.println(solutions.isPrime(2));
+        System.out.println(solutions.isPrime(0));
+        System.out.println(solutions.isPrime(1));
+        System.out.println(solutions.isPrime(31));
+        
+    }
+}
